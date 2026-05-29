24fbc81e52d2bbc80e01887a9ddb37a8e1e8f72b
prmk70-arch <prmk70@gmail.com>
2026-05-29 23:46:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -298,24 +298,9 @@ print_r([
             'saveType' => 1,
             'header' => [
                 'id' => 'documents.purchase',
-                'date' => date('Y-m-d H:i:s', strtotime($omega['Date'])),
-                'originalNumber' => $omega['Number'],
-                'originalDate' => date('Y-m-d H:i:s', strtotime($omega['Date'])),   
-                'firm' => FIRM_ID,
-                'business' => BUSINESS_ID,
-                'storage' => STORAGE_ID,
-                'person' => PERSON_ID,
-                'contract' => CONTRACT_ID,
-                'currency' => CURRENCY_ID,
-                'amountCur' => $omega['Summ'],
-                'rate' => 1,
-                'taxAccount' => 1,
-                // 'paymentForm' => '1110300000000001',
-                // 'department' => '1101900000000001',
-                // 'state' => '1111500000000005',
-                // 'docMode' => DOCMODE_ID,
-                'posted' => 0
-            ],
+    'date' => date('Y-m-d H:i:s', strtotime($omega['Date'])),
+    'originalNumber' => $omega['Number'],
+],
             'tableParts' => []
         ]
     ]);
