56421e66ec4712ef10ce68995a1d58539f3be4f4
prmk70-arch <prmk70@gmail.com>
2026-05-31 18:41:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-echo "SCRIPT START\n";
-
 header('Content-Type: text/plain; charset=utf-8');
 set_time_limit(300);
 
@@ -278,6 +273,10 @@ function importDocument($docId)
     'posted' => 0
 ]);
    
+    echo "TP GOODS:\n";
+print_r($tpGoods);
+die();
+    
     $doc = dilovod([
         'action' => 'saveObject',
         'params' => [
