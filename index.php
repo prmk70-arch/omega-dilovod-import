4b4799b849df8c7d7828ea68cb76f5fd6d48a1d2
prmk70-arch <prmk70@gmail.com>
2026-06-01 17:24:19 +0300

    Refactor createProduct function and clean up code
    Removed unused variable and added header array in createProduct function.

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-print_r($header);
-
 header('Content-Type: text/plain; charset=utf-8');
 set_time_limit(300);
 
@@ -192,6 +190,27 @@ var_dump($brandId);
 
 function createProduct($code, $name, $brandId)
 {
+   $header = [
+    'id' => 0,
+    'code' => 0,
+    'isGroup' => 0,
+
+    'name' => [
+        'uk' => $name,
+        'ru' => $name
+    ],
+
+    'parent' => '1100300000003465',
+    'mainUnit' => '1103600000000001',
+    'tradeMark' => $brandId,
+    'productNum' => $code,
+    'accPolicy' => '1201200000001002',
+    'specQty' => 1
+];
+
+echo "HEADER TO SAVE:\n";
+print_r($header);
+die();
     $res = dilovod([
         'action' => 'saveObject',
         'params' => [
